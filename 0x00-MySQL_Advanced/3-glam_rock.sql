dbdfe819121be69044164c83ce86c495e57e756e
willyopera <willy4opera@gmail.com>
2024-03-21 19:39:31 -0400

    Added the 3

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -3,4 +3,4 @@
 SELECT band_name AS band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
-ORDER BY lifespan DESC;
+ORDER BY lifespan DESC ;
