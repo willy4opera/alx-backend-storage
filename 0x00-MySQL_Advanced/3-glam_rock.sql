2fdbf509428ea69de316a7998dfe7a24b034ab43
willyopera <willy4opera@gmail.com>
2024-03-21 19:27:51 -0400

    Added the 3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -3,4 +3,4 @@
 SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
     FROM metal_bands
     WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
-    ORDER BY lifespan DESC;
+    ORDER BY lifespan DESC ;
