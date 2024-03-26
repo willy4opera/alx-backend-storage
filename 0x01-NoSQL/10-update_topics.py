c85b6e97d39ea6591ec64f419b9865626c7b0ab8
willyopera <willy4opera@gmail.com>
2024-03-26 14:43:17 -0400

    Added the 10-update_topics.py

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python3
-'''Write a Python function that returns the list of school having
-   a specific topic:
-
-    Prototype: def schools_by_topic(mongo_collection, topic):
-    mongo_collection will be the pymongo collection object
-    topic (string) will be topic searched
+'''Task 10's module.
 '''
 
 
