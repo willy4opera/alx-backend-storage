00fbeda50ca44b9ac8f975ee286a5be7c8ca09bc
willyopera <willy4opera@gmail.com>
2024-03-25 20:34:33 -0400

    Added the 10-update_topics.py

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
+'''Write a Python function that returns the list of school having a specific topic:
+
+    Prototype: def schools_by_topic(mongo_collection, topic):
+    mongo_collection will be the pymongo collection object
+    topic (string) will be topic searched
+'''
 
 
 def update_topics(mongo_collection, name, topics):
-    
+    '''Changes all topics of a collection's document based on the name.
+    '''
     mongo_collection.update_many(
         {'name': name},
         {'$set': {'topics': topics}}
