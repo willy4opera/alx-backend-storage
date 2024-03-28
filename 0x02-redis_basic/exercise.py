76fa4182f779d4bc26a9bc0f68ee3dd2760a6e30
willyopera <willy4opera@gmail.com>
2024-03-28 16:17:39 -0400

    Added the exercise.py for task 2

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -20,9 +20,9 @@ class Cache:
     def store(self, data: Union[str, bytes, int, float]) -> str:
         '''Stores a value in a Redis data storage and returns the key.
         '''
-        data_key = str(uuid.uuid4())
-        self._redis.set(data_key, data)
-        return data_key
+        data_ky = str(uuid.uuid4())
+        self._redis.set(data_ky, data)
+        return data_ky
 
     def get(
             self,
