64844a900483e87b8633911eda40aae2a1b4a1cb
willyopera <willy4opera@gmail.com>
2024-03-29 06:45:34 -0400

    Debugged the web.py file for Task 5 Final

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,34 +1,36 @@
 #!/usr/bin/env python3
 """
-Here, we Implements an expiring web cache and tracker
+web cache and tracker
 """
-from typing import Callable
-from functools import wraps
-import redis
 import requests
-r_store = redis.Redis()
+import redis
+from functools import wraps
+
+store = redis.Redis()
 
 
-def link_count(method: Callable) -> Callable:
-    """Monitors the number of url access to server"""
+def count_url_access(method):
+    """ Decorator counting how many times
+    a URL is accessed """
     @wraps(method)
-    def wrapper(*args, **kwargs):
-        url = args[0]
-        r_store.incr(f"count:{url}")
-        cached = r_store.get(f'{url}')
-        if cached:
-            return cached.decode('utf-8')
-        r_store.setex(f'{url}, 10, {method(url)}')
-        return method(*args, **kwargs)
+    def wrapper(url):
+        cached_key = "cached:" + url
+        cached_data = store.get(cached_key)
+        if cached_data:
+            return cached_data.decode("utf-8")
+
+        count_key = "count:" + url
+        html = method(url)
+
+        store.incr(count_key)
+        store.set(cached_key, html)
+        store.expire(cached_key, 10)
+        return html
     return wrapper
 
 
-@link_count
+@count_url_access
 def get_page(url: str) -> str:
-    """get a page and cache value"""
-    response = requests.get(url)
-    return response.text
-
-
-if __name__ == "__main__":
-    get_page('http://slowwly.robertomurray.co.uk')
+    """ Returns HTML content of a url """
+    res = requests.get(url)
+    return res.text
